e96fc3480de22f0d0440c42ddb9bb80d00ab11a3
shogo kohraku <>
2019-01-04 00:05:52 +0900

    add Time Setting TableView

---
+++ MeditationTimer/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  MeditationTimer
+//
+//  Created by shogo kohraku on 2019/01/03.
+//  Copyright © 2019年 shogo kohraku. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
