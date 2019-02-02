abfbbd17c0f4677c9631d1e162b4bdde5a3e897d
shogo kohraku <>
2019-02-02 13:48:22 +0900

    update TableView

---
+++ MeditationTimer/SectionDetailViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  SectionDetailViewController2.swift
+//  MeditationTimer
+//
+//  Created by shogo kohraku on 2019/01/27.
+//  Copyright © 2019年 shogo kohraku. All rights reserved.
+//
+
+import UIKit
+
+class SectionDetailViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
